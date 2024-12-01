6a9a6619b21acfd75fec1762b3988f9cca277466
igor-devloper <wagnerigor9@gmail.com>
2024-12-01 18:53:02 -0300

    fit: release error

--- app/_data/user-admin/index.tsx
+++ app/_data/user-admin/index.tsx
@@ -1,9 +1,10 @@
 import { auth, clerkClient } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export const userAdmin = async () => {
   const { userId } = await auth();
   if (!userId) {
-    throw new Error("Unauthorized");
+    redirect("/");
   }
   const user = await clerkClient().users.getUser(userId);
   if (user.publicMetadata.role === "admin") {
