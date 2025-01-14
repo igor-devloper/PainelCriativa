5205fc2ca2a933ea8ad4b4e48736283275375857
igor-devloper <wagnerigor9@gmail.com>
2025-01-14 17:51:05 -0300

    fit: releae

--- app/types/transaction.ts
+++ app/types/transaction.ts
@@ -1,4 +1,4 @@
-import { Transaction as PrismaTransaction } from "@prisma/client";
+import { Expense as PrismaTransaction } from "@prisma/client";
 
 export interface Transaction extends PrismaTransaction {
   imageUrl: string[];
