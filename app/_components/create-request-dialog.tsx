badc9b6cd466fb41eb57790e87a3bbbde85f3d7d
igor-devloper <wagnerigor9@gmail.com>
2025-01-23 00:13:03 -0300

    fit: updated name of request dialog

--- app/_components/create-request-dialog.tsx
+++ app/_components/create-request-dialog.tsx
@@ -177,7 +177,7 @@ export function CreateRequestDialog({
               name="name"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Nome</FormLabel>
+                  <FormLabel>Nome do projeto</FormLabel>
                   <FormControl>
                     <Input
                       placeholder="Digite o nome da solicitação"
